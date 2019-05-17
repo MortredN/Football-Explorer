6b520cbef577e6265358ec75ccc7bf7b5a086d2c
MortredNuvema <123havietbach@gmail.com>
2019-05-17 15:58:57 +1000

    First commit. Services TypeScript files finished.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LeagueComponent } from './league/league.component';
+import { LeagueTableComponent } from './league/league-table/league-table.component';
+import { LeagueMatchComponent } from './league/league-match/league-match.component';
+import { TeamComponent } from './team/team.component';
+import { TeamMatchComponent } from './team/team-match/team-match.component';
+import { TeamPositionComponent } from './team/team-position/team-position.component';
+import { TeamSquadComponent } from './team/team-squad/team-squad.component';
+import { MatchComponent } from './match/match.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { 
+    path: 'league/:id',
+    component: LeagueComponent,
+    children: [
+      {path: 'league-table', component: LeagueTableComponent},
+      {path: 'league-match', component: LeagueMatchComponent}
+    ] },
+  { 
+    path: 'team/:id',
+    component: TeamComponent,
+    children: [
+      {path: 'team-match', component: TeamMatchComponent},
+      {path: 'team-position', component: TeamPositionComponent},
+      {path: 'team-squad', component: TeamSquadComponent}
+    ] },
+  { path: 'match/:id', component: MatchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { LeaguenavComponent } from './leaguenav/leaguenav.component';
+import { HomeComponent } from './home/home.component';
+import { CompetitionService } from './competition.service';
+import { LeagueComponent } from './league/league.component';
+import { LeagueService } from './league/league.service';
+import { TeamComponent } from './team/team.component';
+import { TeamService } from './team/team.service';
+import { LeagueTableComponent } from './league/league-table/league-table.component';
+import { LeagueMatchComponent } from './league/league-match/league-match.component';
+import { TeamMatchComponent } from './team/team-match/team-match.component';
+import { TeamPositionComponent } from './team/team-position/team-position.component';
+import { TeamSquadComponent } from './team/team-squad/team-squad.component';
+import { MatchComponent } from './match/match.component';
+import { MatchService } from './match/match.service';
+import { HomeService } from './home/home.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    LeaguenavComponent,
+    HomeComponent,
+    LeagueComponent,
+    TeamComponent,
+    LeagueTableComponent,
+    LeagueMatchComponent,
+    TeamMatchComponent,
+    TeamPositionComponent,
+    TeamSquadComponent,
+    MatchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CompetitionService,
+    HomeService,
+    LeagueService,
+    TeamService,
+    MatchService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
